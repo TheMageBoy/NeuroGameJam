2ec38426b90d8d8b6e42d2df03fb19400b565e00
TheMageBoy <zeroframegamedev@gmail.com>
2024-12-28 09:22:47 +0800

    Fixed Issue with window
    Fixed Issue with window

--- companyproperty/Assets/Scripts/UI_Window.gd
+++ companyproperty/Assets/Scripts/UI_Window.gd
@@ -14,7 +14,7 @@ var offset = Vector2()
 func _input(event: InputEvent):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed and !dragging:
-			offset = get_viewport().get_mouse_position() - panel.get_screen_position();
+			offset = panel.get_screen_position() - get_viewport().get_mouse_position();
 			dragging = true;#Toggle Dragging when clicked
 			#print("Clicking");
 
@@ -22,5 +22,8 @@ func _process(delta: float):#I hope this is equivalent to update?
 	if !Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		dragging = false;
 	if dragging == true:
-		drag_pos = get_viewport().get_mouse_position()+offset;#Drag position is set to mouse position
-		panel.position = drag_pos#Thus, set transform position to drag position
+		drag_pos = get_viewport().get_mouse_position() + offset;#Drag position is set to mouse position
+		panel.global_position = drag_pos#Thus, set transform position to drag position
+		
+func close() -> void:
+	print("put window closes method")
