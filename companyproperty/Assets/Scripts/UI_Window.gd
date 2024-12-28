9706929806de63e2c5cf7a3df3d6d93969ab1ff6
TheMageBoy <zeroframegamedev@gmail.com>
2024-12-28 10:27:35 +0800

    Window dragging fix
    If window is not within mouse pos it will not be dragged

--- companyproperty/Assets/Scripts/UI_Window.gd
+++ companyproperty/Assets/Scripts/UI_Window.gd
@@ -7,13 +7,16 @@ var dragging = false
 var drag_pos = Vector2()
 var offset = Vector2()
 
+var mouse_over = false;
+var last_higlihted = false; # for if input will register to the current window
 
 @onready var node: Node = $".."
 @onready var panel: Panel = $"."
 
 func _input(event: InputEvent):
+	
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed and !dragging:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed and !dragging && mouse_within(get_viewport().get_mouse_position()):
 			offset = panel.get_screen_position() - get_viewport().get_mouse_position();
 			dragging = true;#Toggle Dragging when clicked
 			#print("Clicking");
@@ -25,5 +28,13 @@ func _process(delta: float):#I hope this is equivalent to update?
 		drag_pos = get_viewport().get_mouse_position() + offset;#Drag position is set to mouse position
 		panel.global_position = drag_pos#Thus, set transform position to drag position
 		
+func mouse_within(point):
+	var x = panel.global_position.x
+	var y = panel.global_position.y
+	var x2 = x + self.size.x
+	var y2 = y + self.size.y
+	return point.x >= x and point.x <= x2 and point.y >= y and point.y <= y2
+	
+	
 func close() -> void:
 	print("put window closes method")
--- companyproperty/Assets/Scripts/X_Button.gd
+++ companyproperty/Assets/Scripts/X_Button.gd
@@ -8,6 +8,9 @@ func _ready():
 	button.pressed.connect(self._button_pressed)
 	add_child(button)
 	uiwindow = get_parent() #temporary; pass in in init
+	
+func initialize(parent):
+	uiwindow = parent
 
 func _button_pressed():
 	uiwindow.close();
