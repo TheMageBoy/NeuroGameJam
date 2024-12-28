d8aecf3546809353462775b89ec320a030449210
Ddouble <ddouble.dot.22@gmail.com>
2024-12-28 01:47:16 +0100

    DRAG WORKS :Hypers:
    New problem tho, someone figure out how to make it so drag doesn't only move top left

--- companyproperty/Assets/Scripts/UI_Window.gd
+++ companyproperty/Assets/Scripts/UI_Window.gd
@@ -6,12 +6,19 @@ var lifespan = 0
 var dragging = false
 var drag_pos = Vector2()
 
+
+@onready var node: Node = $".."
+@onready var panel: Panel = $"."
+
 func _input(event: InputEvent):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			dragging = !dragging;#Toggle Dragging when clicked
+			dragging = true;#Toggle Dragging when clicked
+			#print("Clicking");
 
-func _draw():#I hope this is equivalent to update?
+func _process(delta: float):#I hope this is equivalent to update?
+	if !Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		dragging = false;
 	if dragging == true:
 		drag_pos = get_viewport().get_mouse_position();#Drag position is set to mouse position
-		#Thus, set transform position to drag position
+		panel.position = drag_pos#Thus, set transform position to drag position
