04aa422ddb89a35e370d62a0951a16a86855c493
TheMageBoy <zeroframegamedev@gmail.com>
2024-12-28 09:46:03 +0800

    X button
    - Implements close fxn

--- companyproperty/Assets/Scripts/X_Button.gd
+++ companyproperty/Assets/Scripts/X_Button.gd
@@ -7,6 +7,7 @@ func _ready():
 	#button.text = "Click me"
 	button.pressed.connect(self._button_pressed)
 	add_child(button)
+	uiwindow = get_parent() #temporary; pass in in init
 
 func _button_pressed():
-	print("Hello world!")
+	uiwindow.close();
