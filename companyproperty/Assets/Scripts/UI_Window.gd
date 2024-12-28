a1ceacbeb71205ae6e50589948691e36655973b8
Ddouble <ddouble.dot.22@gmail.com>
2024-12-28 01:13:10 +0100

    Commented and added to Mage's UI_Window
    Some suggestions

--- companyproperty/Assets/Scripts/UI_Window.gd
+++ companyproperty/Assets/Scripts/UI_Window.gd
@@ -9,4 +9,9 @@ var drag_pos = Vector2()
 func _input(event: InputEvent):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			get_tree().current_scene()
+			dragging = !dragging;#Toggle Dragging when clicked
+
+func _draw():#I hope this is equivalent to update?
+	if dragging == true:
+		drag_pos = get_viewport().get_mouse_position();#Drag position is set to mouse position
+		#Thus, set transform position to drag position
