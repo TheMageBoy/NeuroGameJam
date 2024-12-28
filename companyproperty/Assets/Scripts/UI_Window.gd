a45f49aaa9153ac2ab8a2ce0c3e89eb26307c65e
Ddouble <ddouble.dot.22@gmail.com>
2024-12-28 01:57:32 +0100

    Dragging offset shenanigans... Send HELP

--- companyproperty/Assets/Scripts/UI_Window.gd
+++ companyproperty/Assets/Scripts/UI_Window.gd
@@ -5,6 +5,7 @@ var lifespan = 0
 
 var dragging = false
 var drag_pos = Vector2()
+var offset = Vector2()
 
 
 @onready var node: Node = $".."
@@ -12,7 +13,8 @@ var drag_pos = Vector2()
 
 func _input(event: InputEvent):
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed and !dragging:
+			offset = get_viewport().get_mouse_position() - panel.get_screen_position();
 			dragging = true;#Toggle Dragging when clicked
 			#print("Clicking");
 
@@ -20,5 +22,5 @@ func _process(delta: float):#I hope this is equivalent to update?
 	if !Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		dragging = false;
 	if dragging == true:
-		drag_pos = get_viewport().get_mouse_position();#Drag position is set to mouse position
+		drag_pos = get_viewport().get_mouse_position()+offset;#Drag position is set to mouse position
 		panel.position = drag_pos#Thus, set transform position to drag position
