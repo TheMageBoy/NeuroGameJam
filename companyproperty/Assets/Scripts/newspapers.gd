e7f4f105c5967bbd8144de58cb7400f5a8a924a0
Dev!ant <100167389+DeviantUser@users.noreply.github.com>
2024-12-30 04:11:15 +0930

    ScrollBar and silly new paper
    as title

--- companyproperty/Assets/Scripts/newspapers.gd
+++ companyproperty/Assets/Scripts/newspapers.gd
@@ -1,6 +1,6 @@
 extends Content
 
-@onready var files: VBoxContainer = $Files
+@onready var files: VBoxContainer = $ScrollContainer/Files
 const TEXT_FILE = preload("res://Assets/Scenes/Content/text_file.tscn")
 # Called when the node enters the scene tree for the first time.
 const NEWSPAPER_FILE = preload("res://Assets/Scenes/newspaper_file.tscn")
@@ -9,8 +9,32 @@ func _ready() -> void:
 		var file_content := FileAccess.open("res://Assets/TextFiles/Newspapers/"+file, FileAccess.READ).get_as_text()
 		var string_array := file_content.split("\n===\n")
 		var news_file_inst := NEWSPAPER_FILE.instantiate()
-		news_file_inst.get_node("HBoxContainer/FileName").text = string_array[0]
+		
+		
+		var news_title : String = ""
+		var news_title_parts := string_array[0].split(" ")
+		for part in news_title_parts:
+			var cur_part := part
+			
+			
+			if !randi()%3:
+				cur_part = "[i]"+cur_part+"[/i]"
+			if !randi()%6:
+				cur_part = "[b]"+cur_part+"[/b]"
+			if !randi()%9:
+				cur_part = "[color=red]"+cur_part+"[/color]"
+			elif !randi()%9:
+				cur_part = "[color=purple]"+cur_part+"[/color]"
+			if !randi()%12:
+				cur_part = "[wave]"+cur_part+"[/wave]"
+			if !randi()%12:
+				cur_part = "[shake]"+cur_part+"[/shake]"
+			if !randi()%12:
+				cur_part = "[pulse]"+cur_part+"[/pulse]"
+			news_title += cur_part+" "
+		news_file_inst.get_node("HBoxContainer/FileName").text = " "+news_title
 		files.add_child(news_file_inst)
+		
 		news_file_inst.pressed.connect(Callable(game, "createWindow").bind(news_file_inst, Vector2(256, 256), false, TEXT_FILE, string_array[1]))
 
 func _on_rich_text_label_meta_clicked(meta: Variant) -> void:
